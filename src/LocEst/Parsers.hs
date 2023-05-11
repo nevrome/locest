d29601d2005758b53f9e466035e21840d0512f95
Clemens Schmid <clemens@nevrome.de>
2023-05-11 16:23:38 +0200

    added a progress indicator

--- src/LocEst/Parsers.hs
+++ src/LocEst/Parsers.hs
@@ -11,6 +11,8 @@ import           Data.Char                            (ord)
 import qualified Data.Conduit.List as ConL
 import qualified Data.Conduit.Algorithms.Async as ConAA
 import qualified Data.Vector as V
+import Conduit (MonadIO, Conduit, liftIO)
+import Data.IORef (newIORef, readIORef, modifyIORef)
 
 -- helper functions
 decodingOptions :: Csv.DecodeOptions
@@ -38,5 +40,26 @@ pipeSpatTempPosConduit inPath outPath f =
         -- .| ConL.map f
         .| ConAA.asyncMapC 5 f
         -- .| Con.conduitVector 100 .| ConAA.asyncMapC 5 (V.map f) .| ConL.concat
+        .| progress
         .| ConCsv.toCsv encodingOptions
         .| Con.sinkFile outPath
+
+progress :: (MonadIO m) => Conduit i m i 
+progress = do
+    counterRef <- liftIO $ newIORef (0 :: Int)
+    ConL.mapM_ $ \val -> do
+        n <- liftIO $ readIORef counterRef
+        liftIO $ logProgress n
+        liftIO $ modifyIORef counterRef (+1)
+    where
+        logProgress :: Int -> IO ()
+        logProgress c
+            |  c `rem` 1000 == 0 = putStrLn $ "Grid points done: " ++ padLeft 7 (show c)
+            -- |  c == 100          = putStrLn $ "Probing successful. Continuing now..."
+            | otherwise = return ()
+
+padLeft :: Int -> String -> String
+padLeft n s
+    | length s >= n = reverse (take n (reverse s))
+    | length s < n = replicate (n - length s) ' ' ++ s
+    | otherwise    = s
