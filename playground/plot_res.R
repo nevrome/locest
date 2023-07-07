dabb0bdf00a5a2a6bcd80488a32c6a661143b264
Clemens Schmid <clemens@nevrome.de>
2023-07-07 15:04:52 +0200

    cleaning up test script/data

--- playground/plot_res.R
+++ playground/plot_res.R
@@ -1,15 +1,10 @@
 library(magrittr)
 library(ggplot2)
 
-# interface test
-system('locest search -i testObs.tsv -g testObs.tsv -t -400 -d "varC1=150+varC2=150" -o testSearch.tsv')
-
 # normal search test
-#system('time locest search -i test2Obs.tsv -g test2Grid.tsv -t "-7000,-6000,-5000" -d "varC1=0.0461299+varC2=0.00014293" -o test2Search.tsv')
-#system('time locest search --configFile "shortSearchTestConfig.txt" --outFile test2Search.tsv')
-system('time locest search --configFile "normalSearchTestConfig.txt"')
+system('time locest search --configFile "normalSearch.conf"')
 
-hu <- readr::read_tsv("test2Search.tsv")
+hu <- readr::read_tsv("test_res/test2Search.tsv")
 
 hu %>%
   ggplot() +
@@ -19,9 +14,9 @@ hu %>%
   coord_fixed()
 
 # one position test
-system('time locest search -i test2Obs.tsv -g test2GridOnePoint.tsv -t "-5750, -5500,-5250, -5000, -4750" -d "varC1=-0.0885337:0.0570383:0.01+varC2=-0.0669435:0.1100580:0.01" -o test2Interpolate.tsv')
+system('time locest search -i test2Obs.tsv -g test2GridOnePoint.tsv -t "-5750, -5500,-5250, -5000, -4750" -d "varC1=-0.0885337:0.0570383:0.01+varC2=-0.0669435:0.1100580:0.01" -o test_res/test2Interpolate.tsv')
 
-hu <- readr::read_tsv("test2Interpolate.tsv")
+hu <- readr::read_tsv("test_res/test2Interpolate.tsv")
 
 hu %>%
   ggplot() +
@@ -32,7 +27,6 @@ hu %>%
 
 
 # crossvalidation position test
-system('time locest crossvalidate -i test2Obs.tsv --testFraction 0.1 --iterations 5 -o test2Crossvalidate.tsv')
+system('time locest crossvalidate -i test2Obs.tsv --testFraction 0.1 --iterations 5 -o test_res/test2Crossvalidate.tsv')
 
-# system('locest search -i test2Obs.tsv -g debugGrid.tsv -t "-4000" -d "varC1=0.0410592+varC2=-0.0223009" -o debugInterpolate.tsv')
-
+# 
