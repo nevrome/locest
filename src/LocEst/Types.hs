d2cbdd5d82c9c7d9b072d604ed1d918acb69b483
Clemens Schmid <clemens@nevrome.de>
2023-08-18 20:38:50 +0200

    Revert "various attempts to optimize parsing of the massive distance table - no success, it's too big - probably I have to work per spat grid id"
    This reverts commit 6e3b1faf452b97d9e403fca714c49ead7cd08504.

--- src/LocEst/Types.hs
+++ src/LocEst/Types.hs
@@ -75,6 +75,10 @@ newtype SpatDistMap = SpatDistMatrixMap {
     _spatDistMatrixMap :: HM.HashMap (String, String) Double
 } deriving (Show)
 
+makeSpatDistMap :: [SpatDistObsGrid] -> SpatDistMap
+makeSpatDistMap xs =
+    SpatDistMatrixMap $ HM.fromList (map (\(SpatDistObsGrid oID gID d) -> ((oID,gID),d)) xs)
+
 data SpatDistObsGrid = SpatDistObsGrid {
       _spatDistObsGridObsID    :: String
     , _spatDistObsGridGridID   :: String
