c44a548f7978c3eaf77f59bf6cb9a6c1b6b3bf3c
Clemens Schmid <clemens@nevrome.de>
2024-01-10 15:59:50 +0100

    improved the definition of the config language invented here and then further developed in currycarbon (not qjanno)

--- src/LocEst/CLI/Interface.hs
+++ src/LocEst/CLI/Interface.hs
@@ -2,6 +2,7 @@
 
 module LocEst.CLI.Interface where
 
+import           LocEst.CLI.ConfigLang
 import           LocEst.CLI.Crossvalidate
 import           LocEst.CLI.Search
 import           LocEst.Types
@@ -11,7 +12,6 @@ import           Data.Function            ((&))
 import           Data.List                (groupBy, singleton)
 import qualified Options.Applicative      as OP
 import qualified Text.Parsec              as P
-import qualified Text.Parsec.Error        as P
 import qualified Text.Parsec.String       as P
 import           Text.Read                (readMaybe)
 
@@ -199,9 +199,9 @@ readTempGridString s =
 
 parseTempGridString :: P.Parser [Int]
 parseTempGridString = do
-    P.try parseIntegerSequence P.<|> parseYearList
+    P.try parseIntSequence P.<|> parseYearList
     where
-        parseYearList = parseVector parseInteger
+        parseYearList = parseVector parseInt
 
 optParseSearchDepVarsPos :: OP.Parser [DepVarsPos]
 optParseSearchDepVarsPos = OP.option (OP.eitherReader readSearchDepVarsPos) (
@@ -289,100 +289,3 @@ parseAlgorithmString = do
 
 parseVarName :: P.Parser String
 parseVarName = P.string "var" <> P.many1 P.alphaNum
-
-parseNamedVector :: P.Parser a -> P.Parser b -> P.Parser [(a,b)]
-parseNamedVector parseKey parseValue =
-    parseVector $ parseKeyValuePair parseKey parseValue
-
-parseKeyValuePair :: P.Parser a -> P.Parser b -> P.Parser (a,b)
-parseKeyValuePair parseKey parseValue = do
-    key <- parseKey
-    consumeEqualSep
-    value <- parseValue
-    return (key, value)
-
-parseVector :: P.Parser a -> P.Parser [a]
-parseVector parseValue = do
-    _ <- P.string "c"
-    _ <- P.char '('
-    _ <- P.spaces
-    res <- P.sepBy parseValue consumeCommaSep
-    _ <- P.spaces
-    _ <- P.char ')'
-    return res
-
-consumeEqualSep :: P.Parser ()
-consumeEqualSep = do
-    _ <- P.spaces *> P.char '=' <* P.spaces
-    return ()
-consumeCommaSep :: P.Parser ()
-consumeCommaSep = do
-    _ <- P.spaces *> P.char ',' <* P.spaces
-    return ()
-
-parseDoubleSequence :: P.Parser [Double]
-parseDoubleSequence = do
-    start <- parseDouble
-    _ <- P.oneOf ":"
-    stop <- parseDouble
-    _ <- P.oneOf ":"
-    by <- parsePositiveFloatNumber
-    return [start,(start+by)..stop]
-
-parseDouble :: P.Parser Double
-parseDouble = do
-    P.try parseNegativeFloatNumber P.<|> parsePositiveFloatNumber
-
-parseNegativeFloatNumber :: P.Parser Double
-parseNegativeFloatNumber = do
-    _ <- P.oneOf "-"
-    i <- parsePositiveFloatNumber
-    return (-i)
-
-parseFraction :: P.Parser Double
-parseFraction = do
-    num <- parsePositiveFloatNumber
-    if num > 1
-    then fail "must be between zero and one"
-    else return num
-
-parsePositiveFloatNumber :: P.Parser Double
-parsePositiveFloatNumber = do
-    num <- parseNumber
-    optionalMore <- P.option "" $ (:) <$> P.char '.' <*> parseNumber
-    return $ read $ num ++ optionalMore
-
-parseIntegerSequence :: P.Parser [Int]
-parseIntegerSequence = do
-    start <- parseInteger
-    _ <- P.oneOf ":"
-    stop <- parseInteger
-    _ <- P.oneOf ":"
-    by <- parsePositiveInteger
-    return [start,(start+by)..stop]
-
-parseInteger :: P.Parser Int
-parseInteger = do
-    P.try parseNegativeInteger P.<|> parsePositiveInteger
-
-parseNegativeInteger :: P.Parser Int
-parseNegativeInteger = do
-    _ <- P.oneOf "-"
-    i <- parsePositiveInteger
-    return (-i)
-
-parsePositiveInteger :: P.Parser Int
-parsePositiveInteger = do
-    read <$> parseNumber
-
-parseNumber :: P.Parser [Char]
-parseNumber = P.many1 P.digit
-
--- helper functions
-
-showParsecErr :: P.ParseError -> String
-showParsecErr err =
-    P.showErrorMessages
-        "or" "unknown parse error"
-        "expecting" "unexpected" "end of input"
-        (P.errorMessages err)
