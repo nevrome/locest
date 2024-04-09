66c139d236fbd6762e8753ad6953e3ba43ef405a
Clemens Schmid <clemens@nevrome.de>
2024-04-09 16:16:12 +0200

    a function for the distance calculation

--- src/LocEst/CLI/Vario.hs
+++ src/LocEst/CLI/Vario.hs
@@ -4,6 +4,7 @@ module LocEst.CLI.Vario where
 
 import LocEst.Parsers
 import LocEst.Types
+import LocEst.Distance
 
 import           System.IO       (hPutStrLn, stderr)
 
@@ -20,11 +21,22 @@ runVario (VarioOptions inObsFile _) = do
     let observations = zipWith setIndex observationsUnindexed [0..]
     -- pairwise distances
     hPutStrLn stderr "Calculating pairwise distances"
-
+    let hu = calculatePairwiseDistances observations
     -- huhu
     hPutStrLn stderr "wip"
 
 calculatePairwiseDistances :: [Observation] -> SUDistMatrix
 calculatePairwiseDistances obs =
     undefined
-    
+
+obsobsDist :: Observation -> Observation -> [(String, Double)]
+obsobsDist
+    (Observation _ _ (HyperPos (IndepSpatTempPos p1) _))
+    (Observation _ _ (HyperPos (IndepSpatTempPos p2) _)) =
+    [("space", spatialDistSpatTempPos p1 p2), ("time", temporalDistSpatTempPos p1 p2)]
+obsobsDist
+    (Observation _ _ (HyperPos (IndepArbitraryDimPos p1) _))
+    (Observation _ _ (HyperPos (IndepArbitraryDimPos p2) _)) =
+    -- this assumes that p1 and p2 have the same order of indep variables
+    zip (getKeys p1) (allDistances (getValues p1) (getValues p2))
+obsobsDist _ _ = error "Impossible state in obsobsDist"
