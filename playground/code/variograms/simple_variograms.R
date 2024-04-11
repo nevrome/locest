e96b3742f0cba6f0f6faec66f2cc30dcbfcd0c9b
Clemens Schmid <clemens@nevrome.de>
2024-04-11 11:53:22 +0200

    used new feature in the variogram test script

--- playground/code/variograms/simple_variograms.R
+++ playground/code/variograms/simple_variograms.R
@@ -13,7 +13,13 @@ obs_linear %>%
 
 readr::write_tsv(obs_linear, "data/variograms/obs_linear.tsv")
 
-system('locest vario -i data/variograms/obs_linear.tsv')
+system('locest vario -i data/variograms/obs_linear.tsv --variogramOutFile data/variograms/vario_linear.tsv')
+
+vario_linear <- readr::read_tsv("data/variograms/vario_linear.tsv")
+
+vario_linear %>%
+  ggplot() +
+  geom_point(aes(bin, semivariance))
 
 ###
 
@@ -29,7 +35,13 @@ obs_constant %>%
 
 readr::write_tsv(obs_constant, "data/variograms/obs_constant.tsv")
 
-system('locest vario -i data/variograms/obs_constant.tsv')
+system('locest vario -i data/variograms/obs_constant.tsv --variogramOutFile data/variograms/vario_constant.tsv')
+
+vario_constant <- readr::read_tsv("data/variograms/vario_constant.tsv")
+
+vario_constant %>%
+  ggplot() +
+  geom_point(aes(bin, semivariance))
 
 ###
 
@@ -45,5 +57,11 @@ obs_sin %>%
 
 readr::write_tsv(obs_sin, "data/variograms/obs_sin.tsv")
 
-system('locest vario -i data/variograms/obs_sin.tsv')
+system('locest vario -i data/variograms/obs_sin.tsv --variogramOutFile data/variograms/vario_sin.tsv')
+
+vario_sin <- readr::read_tsv("data/variograms/vario_sin.tsv")
+
+vario_sin %>%
+  ggplot() +
+  geom_point(aes(bin, semivariance))
 
