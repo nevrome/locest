f1f1f66f3375713b93c0f7935bc358141b3e8a57
Clemens Schmid <clemens@nevrome.de>
2024-05-08 15:35:20 +0200

    first draft of optinally merging variables for the variogram calculation

--- src/LocEst/CLI/Vario.hs
+++ src/LocEst/CLI/Vario.hs
@@ -43,7 +43,7 @@ runVario (VarioOptions inObsFile maybeNrBins outVariogramFile) = do
     let observations = V.zipWith setIndex observationsUnindexedVector (V.fromList [0..])
     -- calculate pairwise distances
     hPutStrLn stderr "Calculating pairwise distances for independent variables"
-    !distsPerIndepVar <- calcIndepVarPairwiseDistances observations
+    !distsPerIndepVar <- calcIndepVarPairwiseDistances True observations
     hPutStrLn stderr "Calculating pairwise distances for dependent variables"
     !distsPerDepVar   <- calcDepVarPairwiseDistances observations
     -- iterate over all permutations of indepVars and depVars to calculate empirical variograms
@@ -141,14 +141,14 @@ makeObsPairs obs =
     in zip [0..] obsPairs
 
 -- distance calculation functions
-calcIndepVarPairwiseDistances :: V.Vector Observation -> IO [(IndepVarName, SUDistMatrix)]
-calcIndepVarPairwiseDistances obs = do
+calcIndepVarPairwiseDistances :: Bool -> V.Vector Observation -> IO [(IndepVarName, SUDistMatrix)]
+calcIndepVarPairwiseDistances merge obs = do
     let obsPairs = makeObsPairs obs
         nrPairs = length obsPairs
         (Observation _ _ (HyperPos indepPos _)) = V.head obs
-    case indepPos of
+    case (indepPos,merge) of
         -- spatiotemporal system
-        IndepSpatTempPos _ -> do
+        (IndepSpatTempPos _,_) -> do
             -- create mutable vectors to write distances directly
             spaceVec <- VUM.new nrPairs
             timeVec  <- VUM.new nrPairs
@@ -159,11 +159,17 @@ calcIndepVarPairwiseDistances obs = do
             timeVecNonMut  <- VU.unsafeFreeze timeVec
             return [("space", SUDistMatrix spaceVecNonMut), ("time", SUDistMatrix timeVecNonMut)]
         -- arbitrary dimension system
-        IndepArbitraryDimPos pos@(ArbitraryDimPos l) -> do
+        (IndepArbitraryDimPos pos@(ArbitraryDimPos l),False) -> do
             arbitraryVecs <- replicateM (length l) (VUM.new nrPairs)
             mapM_ (distArbitrary arbitraryVecs) obsPairs
             arbitraryVecsNonMut <- mapM VU.unsafeFreeze arbitraryVecs
             return $ zipWith (\name vec -> (name, SUDistMatrix vec)) (getKeys pos) arbitraryVecsNonMut
+        -- arbitrary dimensions merged
+        (IndepArbitraryDimPos (ArbitraryDimPos _),True) -> do
+            distVec <- VUM.new nrPairs
+            mapM_ (distArbitraryMerged distVec) obsPairs
+            distVecNonMut <- VU.unsafeFreeze distVec
+            return [("euclideanAcrossIndepVars", SUDistMatrix distVecNonMut)]
     where
         distSpaceTime :: VUM.IOVector Double -> VUM.IOVector Double -> (Int, (Observation, Observation)) -> IO ()
         distSpaceTime
@@ -189,6 +195,17 @@ calcIndepVarPairwiseDistances obs = do
             let arbitraryDists = allDistances (getValues p1) (getValues p2)
             zipWithM_ (`VUM.write` i) arbitraryVecs arbitraryDists
         distArbitrary _ _ = error "Impossible state in indep distance calculation"
+        distArbitraryMerged :: VUM.IOVector Double -> (Int, (Observation, Observation)) -> IO ()
+        distArbitraryMerged
+            distVec
+            (i,
+            (Observation _ _ (HyperPos (IndepArbitraryDimPos p1) _),
+            Observation _ _ (HyperPos (IndepArbitraryDimPos p2) _))
+            ) = do
+            -- this assumes that p1 and p2 have the same order of indep variables
+            let arbitraryDistEuclidean = euclideanDistance (getValues p1) (getValues p2)
+            VUM.write distVec i arbitraryDistEuclidean
+        distArbitraryMerged _ _ = error "Impossible state in indep distance calculation"
 
 calcDepVarPairwiseDistances :: V.Vector Observation -> IO [(DepVarName, SUDistMatrix)]
 calcDepVarPairwiseDistances obs = do
