87dccc9947de147c5b76c19580c9cc58fbc075c5
Clemens Schmid <clemens@nevrome.de>
2024-06-06 16:24:03 +0200

    added input option to run the sampling

--- src/LocEst/CLI/Interface.hs
+++ src/LocEst/CLI/Interface.hs
@@ -235,7 +235,7 @@ optParseCrossOutMode = OP.option (OP.eitherReader readOutMode) (
 optParseCoreOutMode :: OP.Parser CoreOutMode
 optParseCoreOutMode = OP.option (OP.eitherReader readOutMode) (
     OP.long "outMode" <>
-    OP.metavar "Short|Full|Obs(n)" <>
+    OP.metavar "Short|Full|Obs(n)|Samples(n,seed)" <>
     OP.value CoreOutShort
     <> OP.helpDoc ( Just (
                       s2d "The type of output that should be written to the --outFile. \
@@ -306,14 +306,18 @@ optParseCoreOutMode = OP.option (OP.eitherReader readOutMode) (
             case P.runParser parseOutMode () "" s of
                 Left err -> Left $ showParsecErr err
                 Right x  -> Right x
-        parseOutMode = P.try parseShort P.<|> parseFull P.<|> parseObs
+        parseOutMode = P.try parseShort P.<|> parseFull P.<|> parseObs P.<|> parseInterpolSample
         parseShort = P.string "Short" >> return CoreOutShort
         parseFull  = P.string "Full"  >> return CoreOutFull
         parseObs   = do
-            res <- parseRecordType "Obs" $ do
-                s <- parseArgument "n" parseInt
-                return s
-            return (CoreOutObsWeight res)
+            parseRecordType "Obs" $ do
+                n <- parseArgument "n" parseInt
+                return $ CoreOutObsWeight n
+        parseInterpolSample = do
+            parseRecordType "Samples" $ do
+                n <- parseArgument "n" parseInt
+                s <- parseArgumentOptional "seed" parseInt
+                return $ CoreOutInterpolSamples n s
 
 optParseCrossvalConfSeed :: OP.Parser (Maybe Int)
 optParseCrossvalConfSeed = OP.option (Just <$> OP.auto) (
