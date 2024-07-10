77c47b0b94254dd9800c6a7e3e02ab64bed183ab
Clemens Schmid <clemens@nevrome.de>
2024-07-10 15:21:15 +0200

    first working golden test

--- tests/MainSpec.hs
+++ tests/MainSpec.hs
@@ -15,7 +15,7 @@ locestSpec =
   describe "locest" $ do
 
     let goldenTests = [
-            --"01_read_observations"
+            "01_basic_spacetime"
             ]
 
     runTestScripts goldenTests
@@ -23,9 +23,9 @@ locestSpec =
     where
         runTestScripts :: [String] -> Spec
         runTestScripts tests = do
-            forM_ tests $ \test -> do
-                it ("should be executed correctly: " ++ test) $ do
-                    let cp = (shell ("bash " ++ test ++ ".sh")) {
+            forM_ tests $ \testConf -> do
+                it ("should be executed correctly: " ++ testConf) $ do
+                    let cp = (shell $ "locest search --configFile " ++ testConf ++ ".conf") {
                           cwd = Just "tests/golden/",
                           std_out = CreatePipe,
                           std_err = CreatePipe
@@ -34,5 +34,6 @@ locestSpec =
                     hSetBuffering out NoBuffering
                     hSetBuffering err NoBuffering
                     -- compare cli output
-                    outExpected <- readFile $ "tests/golden/outCLI/" ++ test ++ ".out"
-                    liftA2 (++) (hGetContents out) (hGetContents err) `shouldReturn` outExpected
+                    outExpected <- readFile $ "tests/golden/outCLI/" ++ testConf ++ ".out"
+                    liftA2 (\x y -> x ++ filter (/= '\r') y) (hGetContents err) (hGetContents out)
+                        `shouldReturn` outExpected
