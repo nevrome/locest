68d164a8e53d88120e277cb52f116e6029430205
Clemens Schmid <clemens@nevrome.de>
2024-07-21 22:42:05 +0200

    preparation to add further tests

--- tests/MathSpec.hs
+++ tests/MathSpec.hs
@@ -11,9 +11,9 @@ spec = do
     testEqualityFullAndPartialFunctions
 
 testEqualityFullAndPartialFunctions :: Spec
-testEqualityFullAndPartialFunctions = describe "LocEst.MathUtils" $ do
-    prop "should yield identical results for full and partial functions" test_avg
-
+testEqualityFullAndPartialFunctions = describe "LocEst.MathUtils: full and partial functions behave identically" $ do
+    prop "avg == avg_" test_avg
+    prop "varSample == varSample_" test_varSample
     where
         test_avg :: [Double] -> Bool
         test_avg [] = True
@@ -21,3 +21,10 @@ testEqualityFullAndPartialFunctions = describe "LocEst.MathUtils" $ do
             let vxs = VU.fromList xs
                 vxslength = fromIntegral $ VU.length vxs
             in avg_ vxslength vxs == avg xs
+        test_varSample :: [Double] -> Bool
+        test_varSample []  = True
+        test_varSample [_] = True
+        test_varSample xs  =
+            let vxs = VU.fromList xs
+                vxslength = fromIntegral $ VU.length vxs
+            in varSample_ vxslength vxs == varSample xs
