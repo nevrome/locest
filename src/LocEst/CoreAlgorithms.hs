ec4270b086190e22d34ae3fbbf40733dc5595b6c
Clemens Schmid <clemens@nevrome.de>
2025-03-02 15:06:33 +0100

    some more minor refactoring for readability

--- src/LocEst/CoreAlgorithms.hs
+++ src/LocEst/CoreAlgorithms.hs
@@ -38,6 +38,25 @@ coreOutInterpolSamples spatDistUnitScaling depVarVariances coreSupplement
         samplesPerDepVar   = map (\(i,r) -> (i, zipWith3 (getRandomSample obsWithDist r) depVars kernelsPerDepVar variancesPerDepVar)) randIterations
     in V.fromList $ map (\(i,s) -> InterpolationSample sett i (ValuesPerDepVar s)) samplesPerDepVar
 
+getRandomSample ::
+       V.Vector (Observation, IndepVarsDist)
+    -> DepVarsRands
+    -> DepVarName
+    -> KernelOneDepVar
+    -> Double
+    -> (DepVarName, Double)
+getRandomSample obsWithDist depVarsRands depVar kernel variance = do
+    let values      = VU.convert $ V.map (getValue depVar) obsWithDist
+        weights     = VU.convert $ V.map (getWeight kernel) obsWithDist
+        random01    = lookupUnsafe depVarsRands depVar
+        totalWeight = VU.sum weights
+        weightedA   = weightedAvg_ totalWeight values weights
+        weightedVB  = weightedVarBasic_ totalWeight weightedA values weights
+        weightedV   = weightedVar_ variance weightedVB totalWeight
+    case posteriorPredictive_ totalWeight weightedA weightedV of
+        Right distribution -> (depVar, quantile distribution random01)
+        Left _             -> (depVar,nan)
+
 -- interpolation and search application
 coreNormal :: Double -> CoreOutMode -> DepVarVariances -> CoreSupplement -> [DepVarName]
               -> V.Vector Observation -> CorePermutation -> SearchResult
@@ -46,11 +65,11 @@ coreNormal spatDistUnitScaling outMode depVarVariances coreSupplement
     let obsWithDist        = filterObs spatDistUnitScaling coreSupplement sett observations
         kernelsPerDepVar   = map (lookupUnsafe kernelDefinition) depVars
         variancesPerDepVar = map (lookupUnsafe depVarVariances) depVars
-        searchValuesPerDepVar = case searchDepVarPos of
+        searchPerDepVar    = case searchDepVarPos of
             Just (DepVarsPredPosDirect x)    -> Just <$> getValues x
             Just (DepVarsPredPosSearchObs x) -> Just <$> getValues ((_hyposDepVarsPos . _obsPos) x)
             Nothing                          -> replicate (length depVars) Nothing
-        interpolPerDepVar = zipWith4 (interpol obsWithDist) depVars kernelsPerDepVar variancesPerDepVar searchValuesPerDepVar
+        interpolPerDepVar = zipWith4 (interpol obsWithDist) depVars kernelsPerDepVar variancesPerDepVar searchPerDepVar
         interpolRes = case outMode of
             CoreOutShort -> map resOneDepvar2Short interpolPerDepVar
             CoreOutFull  -> interpolPerDepVar
@@ -61,7 +80,7 @@ coreNormal spatDistUnitScaling outMode depVarVariances coreSupplement
          , _srLikelihood      = case mapMaybe getLogLikelihood interpolRes of
             [] -> Nothing
             xs ->
-                let valuesPerDepVar = catMaybes searchValuesPerDepVar
+                let valuesPerDepVar = catMaybes searchPerDepVar
                     depDist = euclideanDistance (map _irodvWeightedAvg interpolRes) valuesPerDepVar
                 in Just SearchLikelihood {
                   _slhEuclideanDep  = depDist
@@ -69,25 +88,6 @@ coreNormal spatDistUnitScaling outMode depVarVariances coreSupplement
                 , _slhProbability   = Nothing
                 }
          }
-
-getRandomSample ::
-       V.Vector (Observation, IndepVarsDist)
-    -> DepVarsRands
-    -> DepVarName
-    -> KernelOneDepVar
-    -> Double
-    -> (DepVarName, Double)
-getRandomSample obsWithDist depVarsRands depVar kernel variance = do
-    let values  = VU.convert $ V.map (getValue depVar) obsWithDist
-        weights = VU.convert $ V.map (getWeight kernel) obsWithDist
-        random01 = lookupUnsafe depVarsRands depVar
-        totalWeight = VU.sum weights
-        weightedA   = weightedAvg_ totalWeight values weights
-        weightedVBasic = weightedVarBasic_ totalWeight weightedA values weights
-        weightedV   = weightedVar_ variance weightedVBasic totalWeight
-    case posteriorPredictive_ totalWeight weightedA weightedV of
-        Right distribution -> (depVar, quantile distribution random01)
-        Left _             -> (depVar,nan)
 
 interpol ::
        V.Vector (Observation, IndepVarsDist)
@@ -97,13 +97,13 @@ interpol ::
     -> Maybe Double
     -> InterpolationResultOneDepVar
 interpol obsWithDist depVar kernel variance maybeSearchValue = do
-    let values  = VU.convert $ V.map (getValue depVar) obsWithDist
-        weights = VU.convert $ V.map (getWeight kernel) obsWithDist
+    let values      = VU.convert $ V.map (getValue depVar) obsWithDist
+        weights     = VU.convert $ V.map (getWeight kernel) obsWithDist
         totalWeight = VU.sum weights
         neff        = totalWeight
         weightedA   = weightedAvg_ totalWeight values weights
-        weightedVBasic = weightedVarBasic_ totalWeight weightedA values weights
-        weightedV   = weightedVar_ variance weightedVBasic totalWeight
+        weightedVB  = weightedVarBasic_ totalWeight weightedA values weights
+        weightedV   = weightedVar_ variance weightedVB totalWeight
     case posteriorPredictive_ totalWeight weightedA weightedV of
         Right distribution ->
             let lower  = quantile distribution 0.025
@@ -111,17 +111,17 @@ interpol obsWithDist depVar kernel variance maybeSearchValue = do
                 upper  = quantile distribution 0.975
                 logL   = fmap (logDensity distribution) maybeSearchValue -- log-likelihood
             in InterpolationResultOneDepVarFull
-                depVar neff weightedA weightedVBasic weightedV (OutBool True)
+                depVar neff weightedA weightedVB weightedV (OutBool True)
                 (OutInfDouble lower) median (OutInfDouble upper) logL
         Left _ ->
             case maybeSearchValue of
                 Just _ -> -- requires a proper prior
                     InterpolationResultOneDepVarFull
-                        depVar neff weightedA weightedVBasic weightedV (OutBool False)
+                        depVar neff weightedA weightedVB weightedV (OutBool False)
                         (OutInfDouble (-infinity)) weightedA (OutInfDouble infinity) (Just (-infinity))
                 Nothing ->
                     InterpolationResultOneDepVarFull
-                        depVar neff weightedA weightedVBasic weightedV (OutBool False)
+                        depVar neff weightedA weightedVB weightedV (OutBool False)
                         (OutInfDouble (-infinity)) weightedA (OutInfDouble infinity) Nothing
 
 getValue :: DepVarName -> (Observation,IndepVarsDist) -> Double
