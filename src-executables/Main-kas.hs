a700e4e3ada9dd16d058ca1d76a4f8b9e8b78f9a
Clemens Schmid <clemens@nevrome.de>
2025-03-19 22:37:05 +0100

    further work on the kas hmatrix implementation

--- src-executables/Main-kas.hs
+++ src-executables/Main-kas.hs
@@ -5,29 +5,29 @@ import           Statistics.Distribution.StudentT  (StudentT,
                                                     studentTUnstandardized)
 import           Statistics.Distribution.Transform (LinearTransform)
 import           Statistics.Distribution (logDensity, quantile)
-import System.Environment (getArgs)
 import Control.Monad (when)
-import System.Exit (exitFailure)
 import Prelude  hiding ((<>))
 import Data.List (singleton)
 
 main :: IO ()
 main = do
-    args <- getArgs  -- Get the list of arguments
-    when (length args /= 3) $ do
-        putStrLn "Usage: program <x_value> <y_value> <t_value>"
-        exitFailure
     -- Convert the arguments to the appropriate types
-    let x = (read $ args !! 1) :: [Double]
-        y = (read $ args !! 2) :: [Double]
-        t = (read $ args !! 3) :: Double
-        
-    let xx = undefined
+    let x = fromList [ 0.6152178, 6.2557499, 2.0140909, 2.4989887, 3.4760168, 4.9998595
+            , 5.6690043, 5.2020506, 3.4362923, 0.2459829, 4.1285186, 5.6877753
+            , 3.7348048, 5.6415202, 1.3087586, 4.7733041, 4.0351114, 6.2180439
+            , 2.3231876, 2.2225948
+            ]
+        y = fromList [  0.6560150, -0.1904953,  0.7284932,  0.6543980, -0.4639319, -1.0816896
+            , -0.2914018, -0.8762713, -0.3765554,  0.7665486, -0.9453003, -0.4434826
+            , -0.9415059, -0.5509706,  0.9326286, -1.1088899, -0.7854040,  0.1428688
+            ,  0.6361191,  1.3020555
+            ]
+        t = 3
+        xx = linspace 100 (0, 2*pi)
     
-    let (lower, median, upper) = kernelAverageSmoothing x y t xx
-    putStrLn $ "Lower: " ++ show lower
-    putStrLn $ "Median: " ++ show median
-    putStrLn $ "Upper: " ++ show upper
+    let res = kernelAverageSmoothing x y t xx
+    
+    putStrLn $ show res
 
 -- calculate square of Euclidean norm
 sqEuclideanNorm :: Matrix R -> Matrix R -> R
@@ -45,25 +45,28 @@ vectorMean :: Vector R -> R
 vectorMean v = sumElements v / fromIntegral (size v)
 
 -- Function for computing kernel average smoothing
-kernelAverageSmoothing :: [Double] -> [Double] -> Double -> [Double] -> (Vector R, Vector R, Vector R)
+kernelAverageSmoothing :: Vector R -> Vector R -> Double -> Vector R -> [(Double, Double, Double)]
 kernelAverageSmoothing x y t xx = do
-    --distribution <- forM [1 .. rows xx] $ \i ->
-    --  generalizedStudentT (mu ! i) (scale ! i) dof
-    --let lower  = quantile distribution 0.025
-    --    median = quantile distribution 0.5
-    --    upper  = quantile distribution 0.975
-    (mu, scale, dof)
+    zipWith3 queryDistribution (toList mu) (toList scale) (toList dof)
     where
-      values = fromRows $ replicate (length xx) (fromList y)
-      dists = distanceMatrix (fromColumns $ singleton $ fromList xx) (fromColumns $ singleton $ fromList x) -- + scalar 0.01
+      values = fromRows $ replicate (size xx) y
+      dists = distanceMatrix (fromColumns [xx]) (fromColumns [x]) -- + scalar 0.01
       weights = cmap (exp . negate) (dists / scalar t ^ 2)
       totalWeight = rowSums weights
       weightedAvg = rowSums (values * weights) / totalWeight
       weightedVarBasic = rowSums (weights * (values - asColumn weightedAvg) ^ 2) / (totalWeight - 1)
       scaledS2 = (totalWeight - 1) * weightedVarBasic
-      varSample = sumElements (fromList (map (\x-> (x - avg y)^2) y)) / (fromIntegral (length y) - 1)
+      varSample = sumElements (cmap (\x-> (x - vectorMean y)^2) y) / (fromIntegral (size y) - 1)
       weightedVar = cmap (+ varSample) scaledS2 / (1 + totalWeight)
       mu = weightedAvg
       scale = sqrt $ (1 + 1 / totalWeight) * weightedVar
       dof = totalWeight
-    
+      queryDistribution :: Double -> Double -> Double -> (Double, Double, Double)
+      queryDistribution _mu _scale _dof = 
+          case generalizedStudentT _mu _scale _dof of
+              Right distribution ->
+                  let lower  = quantile distribution 0.025
+                      median = quantile distribution 0.5
+                      upper  = quantile distribution 0.975
+                  in (lower, median, upper)
+              Left e -> error $ show e
