8bd00e9c1876fb0571ebf58aec3ed4149aa8029e
Clemens Schmid <nevromecs@gmail.com>
2026-01-20 14:57:52 +0100

    input distance matrix handling in cross

--- src/LocEst/CLI/Cross.hs
+++ src/LocEst/CLI/Cross.hs
@@ -20,6 +20,7 @@ import           System.IO                     (hPutStrLn, stderr)
 import           System.Random                 as R
 import LocEst.CLI.Search (search, Permutation (..))
 import Data.Maybe (fromMaybe)
+import LocEst.TypesFlat
 
 data CrossOptions = CrossOptions
     { _crossInObservationFile2  :: FilePath
@@ -37,9 +38,9 @@ runCross (
     inObsFile
     testAlgorithms
     testFraction iterations maybeSeed
-    _ -- maybeObsObsDistFile
+    maybeObsObsDistFile
     outFile
-    ) _ --spatDistUnitScaling
+    ) spatDistUnitScaling
     = do
     -- algorithm settings
     let kernelDefinition = head testAlgorithms
@@ -54,7 +55,7 @@ runCross (
     let nObs = V.length obs
     hPutStrLn stderr $ "Number of observations: " ++ show nObs
     -- read distances
-    -- !obsObsDistances <- traverse (readSUDistMulti nObs) maybeObsObsDistFile
+    !obsObsDistances <- traverse (readSUDistMulti nObs) maybeObsObsDistFile
     -- reporting split size
     let numTestObs = round $ testFraction * fromIntegral nObs
     hPutStrLn stderr $ "Number of test observations with fraction" ++ show testFraction ++ ": " ++ show nObs
@@ -68,31 +69,40 @@ runCross (
         .| ConC.mapM_ (\iter ->
                Con.runConduit $
                       ConC.yieldMany testAlgorithms
-                   .| ConC.concatMapM (liftIO . cross algorithm indepVars baseSeed numTestObs iter obs)
+                   .| ConC.concatMapM (liftIO . cross spatDistUnitScaling algorithm indepVars obsObsDistances baseSeed numTestObs iter obs)
                    .| progress 1000 Nothing
                    .| sinkNamedCSV outFile
            )
     putStrLn "Done"
 
 cross
-  :: Algorithm
+  :: Double
+  -> Algorithm
   -> [IndepVarName]
+  -> Maybe SUDistMatrixPerIndepVar
   -> Int -- base seed
   -> Int -- numTestObs
   -> Int -- iteration
   -> V.Vector Observation
   -> KernelDefinition
   -> IO [CrossvalOutput]
-cross algorithm indepVars seed numTestObs iteration obs kerndef = do
+cross spatDistUnitScaling algorithm indepVars maybeFullObsObsDists seed numTestObs iteration obs kerndef = do
     let seedIter = seed + iteration
-        (testObs, trainingObs) = splitTestTraining seedIter numTestObs obs
         depVars   = getKeys kerndef
         kernels   = getValues kerndef
+        -- randomly slice training and test
+        (testIdx, trainIdx) = splitIdx seedIter numTestObs (V.length obs)
+        testObs     = V.backpermute obs testIdx
+        trainingObs = V.backpermute obs trainIdx
         -- prediction grid = test observation locations
         predGrid  = V.map posFromObs testObs
         trueVals = V.map (filterByKey depVars . depVarPosFromObs) testObs
+        -- slice distance matrices, if present
+        !maybeObsObsDists = sliceSUDistPerIndep trainIdx <$> maybeFullObsObsDists
+        !maybeGridGridDists = sliceSUDistPerIndep testIdx <$> maybeFullObsObsDists
+        !maybeObsGridDists = sliceAUDistPerIndep testIdx trainIdx <$> maybeFullObsObsDists
     -- run search (no dep search grid, no temp grid)
-    rows <- search algorithm indepVars Nothing Nothing Nothing 1.0 depVars kernels
+    rows <- search algorithm indepVars maybeObsGridDists maybeObsObsDists maybeGridGridDists spatDistUnitScaling depVars kernels
                    (Permutation iteration trainingObs predGrid (Just trueVals) Nothing)
     -- align rows with true values
     let perObs = zip rows (V.toList trueVals)
@@ -127,3 +137,66 @@ depEuclidean :: DepVarsPos -> DepVarsPos -> Double
 depEuclidean (ValuesPerDepVar _ v1) (ValuesPerDepVar _ v2) =
     sqrt . VS.sum $ VS.zipWith (\x y -> (x - y)^(2 :: Int)) v1 v2
 
+splitIdx :: Int -> Int -> Int -> (V.Vector Int, V.Vector Int)
+splitIdx seed nTest n =
+  let rng = R.mkStdGen seed
+      idxs = V.fromList [0..n-1]
+      (shuffled,_) = shuffle idxs rng
+  in V.splitAt nTest shuffled
+
+{-# INLINE idxHalf #-}
+idxHalf :: Int -> Int -> Int
+idxHalf i j
+  | i >= j    = i*(i+1) `div` 2 + j
+  | otherwise = j*(j+1) `div` 2 + i
+
+sliceSUDistMatrix
+    :: V.Vector Int
+    -> SUDistMatrix
+    -> SUDistMatrix
+sliceSUDistMatrix idxs (SUDistMatrix full) =
+  let n = V.length idxs
+      nHalf = n*(n+1) `div` 2
+      v = VS.generate nHalf $ \k ->
+            let (i,j) = unHalf k
+                oi = idxs V.! i
+                oj = idxs V.! j
+            in full VS.! idxHalf oi oj
+  in SUDistMatrix v
+  where
+    unHalf k =
+      let i = floor ((sqrt (8*fromIntegral k + 1) - 1) / 2)
+          j = k - i*(i+1) `div` 2
+      in (i,j)
+
+sliceAUDistMatrix
+  :: V.Vector Int   -- test indices (rows)
+  -> V.Vector Int   -- train indices (cols)
+  -> SUDistMatrix   -- full obs–obs
+  -> AUDistMatrix
+sliceAUDistMatrix testIdx trainIdx (SUDistMatrix full) =
+  let nGrid = V.length testIdx
+      nObs  = V.length trainIdx
+      v = VS.generate (nGrid * nObs) $ \k ->
+            let (g,o) = k `divMod` nObs
+                oi = trainIdx V.! o
+                gi = testIdx  V.! g
+            in full VS.! idxHalf oi gi
+  in AUDistMatrix nObs nGrid v
+
+sliceSUDistPerIndep
+  :: V.Vector Int
+  -> SUDistMatrixPerIndepVar
+  -> SUDistMatrixPerIndepVar
+sliceSUDistPerIndep idxs (SUDistMatrixPerIndepVar ms) =
+  SUDistMatrixPerIndepVar
+    [ (name, sliceSUDistMatrix idxs m) | (name,m) <- ms ]
+
+sliceAUDistPerIndep
+  :: V.Vector Int
+  -> V.Vector Int
+  -> SUDistMatrixPerIndepVar
+  -> AUDistMatrixPerIndepVar
+sliceAUDistPerIndep testIdx trainIdx (SUDistMatrixPerIndepVar ms) =
+  AUDistMatrixPerIndepVar
+    [ (name, sliceAUDistMatrix testIdx trainIdx m) | (name,m) <- ms ]
