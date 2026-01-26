2c078cbb6a6d294dcab752e0cdf43f4718fc2f9b
Clemens Schmid <nevromecs@gmail.com>
2026-01-26 18:17:36 +0100

    playing around with the test code

--- playground/code/spatiotemporal/search_stuttgart.R
+++ playground/code/spatiotemporal/search_stuttgart.R
@@ -20,7 +20,7 @@ obsGridDists <- fields::rdist(
   #dplyr::transmute(obsID, gridID, space = value/1000)
   dplyr::transmute(space = round(value/1000, 1))
 readr::write_tsv(obsGridDists, "data/spatiotemporal/obsGridDistFile.tsv")
-system('time locest serialise audist -i data/spatiotemporal/obs.tsv -g data/spatiotemporal/grid.tsv --distFile data/spatiotemporal/obsGridDistFile.tsv -o data/spatiotemporal/obsGridDistFile.cbor')
+system('time locest serialise crossdist -i data/spatiotemporal/obs.tsv -g data/spatiotemporal/grid.tsv --distFile data/spatiotemporal/obsGridDistFile.tsv -o data/spatiotemporal/obsGridDistFile.cbor')
 
 space_mat <- fields::rdist(as.matrix(obs[c("x","y")])) / 1000
 n <- nrow(obs)
@@ -32,7 +32,7 @@ obsObsPacked <- tibble::tibble(
   space = round(space_mat[cbind(pairs[, "row"] + 1, pairs[, "col"] + 1)], 1)
 )
 readr::write_tsv(obsObsPacked, "data/spatiotemporal/obsObsDistFile.tsv")
-system('time locest serialise sudist -i data/spatiotemporal/obs.tsv --distFile data/spatiotemporal/obsObsDistFile.tsv -o data/spatiotemporal/obsObsDistFile.cbor')
+system('time locest serialise selfdist -i data/spatiotemporal/obs.tsv --distFile data/spatiotemporal/obsObsDistFile.tsv -o data/spatiotemporal/obsObsDistFile.cbor')
 
 space_mat <- fields::rdist(as.matrix(grid[c("x","y")])) / 1000
 n <- nrow(grid)
@@ -44,7 +44,7 @@ gridGridPacked <- tibble::tibble(
   space = round(space_mat[cbind(pairs[, "row"] + 1, pairs[, "col"] + 1)], 1)
 )
 readr::write_tsv(gridGridPacked, "data/spatiotemporal/gridGridDistFile.tsv")
-system('time locest serialise sudist -g data/spatiotemporal/grid.tsv --distFile data/spatiotemporal/gridGridDistFile.tsv -o data/spatiotemporal/gridGridDistFile.cbor')
+system('time locest serialise selfdist -g data/spatiotemporal/grid.tsv --distFile data/spatiotemporal/gridGridDistFile.tsv -o data/spatiotemporal/gridGridDistFile.cbor')
 
 # stack install --profile
 # stack exec --profile -- locest vario --obsFile data/spatiotemporal/obs.tsv --variogramOutFile data/spatiotemporal/vario.tsv +RTS -hc -l
@@ -54,9 +54,9 @@ system('time locest serialise sudist -g data/spatiotemporal/grid.tsv --distFile 
 
 system('time locest vario --obsFile data/spatiotemporal/obs.tsv --outMode "EqualSize(100)" --outFile data/spatiotemporal/vario.tsv --across AllCombinations')
 
-vario <- readr::read_tsv("data/spatiotemporal/vario.tsv")
+vario_res <- readr::read_tsv("data/spatiotemporal/vario.tsv")
 
-vario %>%
+vario_res %>%
   ggplot() +
   geom_point(aes(bin_mid, variance)) +
   facet_grid(
@@ -72,9 +72,9 @@ vario %>%
 
 system('time OMP_NUM_THREADS=4 locest cross --configFile code/spatiotemporal/cross.conf')
 
-cross <- readr::read_tsv("data/spatiotemporal/cross.tsv")
+cross_res <- readr::read_tsv("data/spatiotemporal/cross.tsv")
 
-cross %>%
+cross_res %>%
   dplyr::group_by(depVar, kernel_space_length, kernel_time_length) %>%
   dplyr::summarise(
     dplyr::across(
@@ -100,19 +100,19 @@ cross %>%
 # OMP_NUM_THREADS=4 stack exec --profile -- locest search --configFile code/spatiotemporal/basic.conf +RTS -hy -RTS
 # hp2ps -c locest.hp
 
-system('time OMP_NUM_THREADS=4 locest search --configFile code/spatiotemporal/basic.conf')
+system('time OMP_NUM_THREADS=20 locest search --configFile code/spatiotemporal/basic.conf')
 
 # better memory profiling with GNU time
 # export TIME="time result\ncmd: %C\nreal %es\nuser %Us \nsys  %Ss \nmemory: %MKB \ncpu: %P"
 # OMP_NUM_THREADS=4 /usr/bin/time locest search --configFile code/spatiotemporal/basic.conf
 
-hu5 <- readr::read_tsv("data/spatiotemporal/basic_result.tsv")
+search_res <- readr::read_tsv("data/spatiotemporal/basic_result.tsv")
 
 # normalization sanity check
-hu5 %>% dplyr::group_by(search_obsID, grid_yearBCAD) %>%
+search_res %>% dplyr::group_by(search_obsID, grid_yearBCAD) %>%
   dplyr::summarize(hu = sum(search_probability))
 
-hu5 %>%
+search_res %>%
   dplyr::filter(temp_sampling_iteration == 0) %>%
   ggplot() +
   facet_grid(rows = dplyr::vars(grid_yearBCAD), cols = dplyr::vars(search_obsID)) +
